fef9cd14d5f0c459537e1b994db3b0a857ddc2e5
Ubuntu <ubuntu@ip-172-31-28-236.ec2.internal>
2024-12-02 10:47:55 +0000

    python app

--- src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-// File: HelloWorld.java
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-         System.out.println("Hello, World om saraswathi namaha!");
-    }
-}
-
